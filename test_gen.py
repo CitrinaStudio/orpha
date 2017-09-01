e1bc9e9513929a3619c8da40e1af13fbcbb18f89
Phon Phey <phonphey@o2.pl>
2017-09-01 21:20:01 +0300

    heh

--- test_gen.py
+++ test_gen.py
@@ -1,28 +1,3 @@
-import timeit
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
+import lib
 
-
-sns.set(color_codes=True)
-
-setup = '''
-import numpy
-
-import gen
-    
-params = gen.gen_name(20)
-crc32 = gen.gen_crc32_hash(params)
-adler32 = gen.gen_adler32_hash(params)
-
-print(params)
-print(crc32)
-print(adler32)
-
-'''
-
-time = np.asarray(timeit.Timer(setup=setup).repeat(1000))
-print(time)
-iris = sns.load_dataset("iris")
-sns.distplot(time)
-plt.show()
+print(lib.gen.gen_adler32_hash(6544))
