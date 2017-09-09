033067512e456dd6fe703a489fdcea563cbf6307
KDV <kdv200330@bk.ru>
2017-09-09 22:38:17 +0300

    made map and new player params

--- header.py
+++ header.py
@@ -13,52 +13,74 @@ CLASSES_BONUSES = {
         'hp': 5,
         'mp': 0,
         'str': 5,
-        'inte': 0
+        'dex': 1,
+        'con': 1,
+        'inte': 1,
+        'wis': 1,
+        'chа': 1
+
     },
     'Roge': {
         'hp': 4,
         'mp': 1,
-        'dex': 5
+        'str': 1,
+        'dex': 5,
+        'con': 1,
+        'inte': 1,
+        'wis': 1,
+        'cha': 1
     },
     'Mage': {
         'hp': 2,
         'mp': 5,
-        'wis': 5
+        'str': 1,
+        'dex': 1,
+        'con': 1,
+        'inte': 1,
+        'wis': 5,
+        'cha': 1
     }
 }
 
 CLASSES_ABILITY = {
     'Warrior':{
-        'str': np.random.rand(4,10),
-        'dex': np.random.rand(2,5),
-        'con': np.random.rand(4,9),
-        'inte': np.random.rand(1,3),
-        'wis': np.random.rand(1,3),
-        'cha': np.random.rand(2,5)
+        'str': np.random.randint(4,10),
+        'dex': np.random.randint(2,5),
+        'con': np.random.randint(4,9),
+        'inte': np.random.randint(1,3),
+        'wis': np.random.randint(1,3),
+        'cha': np.random.randint(2,5)
     },
     'Roge':{
-        'str': np.random.rand(4,7),
-        'dex': np.random.rand(4,9),
-        'con': np.random.rand(3,5),
-        'inte': np.random.rand(1,4),
-        'wis': np.random.rand(1,4), 
-        'cha': np.random.rand(2,5)
+        'str': np.random.randint(4,7),
+        'dex': np.random.randint(4,9),
+        'con': np.random.randint(3,5),
+        'inte': np.random.randint(1,4),
+        'wis': np.random.randint(1,4), 
+        'cha': np.random.randint(2,5)
     },
     'Mage':{
-        'str': np.random.rand(2,6),
-        'dex': np.random.rand(2,5),
-        'con': np.random.rand(2,5),
-        'inte': np.random.rand(5,10),
-        'wis': np.random.rand(5,10),
-        'cha': np.random.rand(2,6) 
+        'str': np.random.randint(2,6),
+        'dex': np.random.randint(2,5),
+        'con': np.random.randint(2,5),
+        'inte': np.random.randint(5,10),
+        'wis': np.random.randint(5,10),
+        'cha': np.random.randint(2,6) 
     }
 }
 
 TABLES_CREATE_COMMANDS = {
     'lands': "CREATE TABLE `lands` (`name` TEXT NOT NULL UNIQUE,`entry_point` TEXT NOT NULL UNIQUE);",
-    'players': "CREATE TABLE `players` ( `hash`	TEXT NOT NULL UNIQUE, `name` TEXT UNIQUE NOT NULL, `age` INTEGER NOT NULL, `class` TEXT NOT NULL,	`coor` TEXT NOT NULL, `hp` TEXT NOT NULL, `mp` TEXT NOT NULL, 'str' INTEGER NOT NULL, 'dex' INTEGER NOT NULL, 'con' INTEGER NOT NULL, 'inte' INTEGER NOT NULL, 'wis' INTEGER NOT NULL,  'cha' INTEGER NOT NULL, PRIMARY KEY(`hash`));",
+    'players': "CREATE TABLE `players` ( `hash`	TEXT NOT NULL UNIQUE, `name` TEXT UNIQUE NOT NULL, `age` INTEGER NOT NULL, `class` TEXT NOT NULL,	`coor` TEXT NOT NULL, `hp` TEXT NOT NULL, `mp` TEXT NOT NULL, `str` INTEGER NOT NULL, `dex` INTEGER NOT NULL, `con` INTEGER NOT NULL, `inte` INTEGER NOT NULL, `wis` INTEGER NOT NULL,  `cha` INTEGER NOT NULL, PRIMARY KEY(`hash`));",
     'bars': "CREATE TABLE `bars` (`coor_hash` TEXT NOT NULL UNIQUE,	`name` TEXT NOT NULL, `menu` TEXT, PRIMARY KEY(`coor_hash`));",
-    'homes': "CREATE TABLE `homes` (`coor_hash`	TEXT NOT NULL UNIQUE, `detail` TEXT, PRIMARY KEY(`coor_hash`));"
+    'homes': "CREATE TABLE `homes` (`coor_hash`	TEXT NOT NULL UNIQUE, `detail` TEXT, PRIMARY KEY(`coor_hash`));",
+    'rivers': "CREATE TABLE `rivers` (`coor_hash` TEXT NOT NULL UNIQUE,`name` TEXT NOT NULL, PRIMARY KEY(`coor_hash`));",
+    'shops': "CREATE TABLE `shops` (`coor_hash` TEXT NOT NULL UNIQUE,`name` TEXT NOT NULL, PRIMARY KEY(`coor_hash`));",
+    'mountains': "CREATE TABLE `mountains` (`coor_hash` TEXT NOT NULL UNIQUE,`name` TEXT NOT NULL, PRIMARY KEY(`coor_hash`));",
+    'caves': "CREATE TABLE `caves` (`coor_hash` TEXT NOT NULL UNIQUE,`name` TEXT NOT NULL, PRIMARY KEY(`coor_hash`));",
+    'fields': "CREATE TABLE `fields` (`coor_hash` TEXT NOT NULL UNIQUE,`name` TEXT NOT NULL, PRIMARY KEY(`coor_hash`));",
+    'forests': "CREATE TABLE `forests` (`coor_hash` TEXT NOT NULL UNIQUE,`name` TEXT NOT NULL, PRIMARY KEY(`coor_hash`));"
+    
 }
 
 
