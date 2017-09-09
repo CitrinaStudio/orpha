4e1e5d3226f92f3f84ed25e9f0ed8c1686bf5173
KDV <kdv200330@bk.ru>
2017-09-09 17:45:26 +0300

    player_params

--- header.py
+++ header.py
@@ -1,5 +1,7 @@
 """ Header Module """
 
+import numpy as np
+
 PLAYER_CLASSES = ("Warrior", "Roge", "Mage")
 QUANTITY_PLAYER_CLASSES = len(PLAYER_CLASSES)
 
@@ -9,48 +11,52 @@ DEFAULT_HP = 10
 CLASSES_BONUSES = {
     'Warrior': {
         'hp': 5,
-        'mp': 0
+        'mp': 0,
+        'str': 5,
+        'inte': 0
     },
     'Roge': {
         'hp': 4,
-        'mp': 1
+        'mp': 1,
+        'dex': 5
     },
     'Mage': {
         'hp': 2,
-        'mp': 5
+        'mp': 5,
+        'wis': 5
     }
 }
 
 CLASSES_ABILITY = {
     'Warrior':{
-        'str': 0,
-        'dex': 0,
-        'con': 0,
-        'int': 0,
-        'wis': 0,
-        'char': 0
+        'str': np.random.rand(4,10),
+        'dex': np.random.rand(2,5),
+        'con': np.random.rand(4,9),
+        'inte': np.random.rand(1,3),
+        'wis': np.random.rand(1,3),
+        'cha': np.random.rand(2,5)
     },
     'Roge':{
-        'str': 0,
-        'dex': 0,
-        'con': 0,
-        'int': 0,
-        'wis': 0, 
-        'char': 0 
+        'str': np.random.rand(4,7),
+        'dex': np.random.rand(4,9),
+        'con': np.random.rand(3,5),
+        'inte': np.random.rand(1,4),
+        'wis': np.random.rand(1,4), 
+        'cha': np.random.rand(2,5)
     },
     'Mage':{
-        'str': 0,
-        'dex': 0,
-        'con': 0,
-        'int': 0,
-        'wis': 0,
-        'char': 0 
+        'str': np.random.rand(2,6),
+        'dex': np.random.rand(2,5),
+        'con': np.random.rand(2,5),
+        'inte': np.random.rand(5,10),
+        'wis': np.random.rand(5,10),
+        'cha': np.random.rand(2,6) 
     }
 }
 
 TABLES_CREATE_COMMANDS = {
     'lands': "CREATE TABLE `lands` (`name` TEXT NOT NULL UNIQUE,`entry_point` TEXT NOT NULL UNIQUE);",
-    'players': "CREATE TABLE `players` ( `hash`	TEXT NOT NULL UNIQUE, `name` TEXT UNIQUE NOT NULL, `age` INTEGER NOT NULL, `class` TEXT NOT NULL,	`coor` TEXT NOT NULL, `hp` TEXT NOT NULL, `mp` TEXT NOT NULL,  PRIMARY KEY(`hash`));",
+    'players': "CREATE TABLE `players` ( `hash`	TEXT NOT NULL UNIQUE, `name` TEXT UNIQUE NOT NULL, `age` INTEGER NOT NULL, `class` TEXT NOT NULL,	`coor` TEXT NOT NULL, `hp` TEXT NOT NULL, `mp` TEXT NOT NULL, 'str' INTEGER NOT NULL, 'dex' INTEGER NOT NULL, 'con' INTEGER NOT NULL, 'inte' INTEGER NOT NULL, 'wis' INTEGER NOT NULL,  'cha' INTEGER NOT NULL, PRIMARY KEY(`hash`));",
     'bars': "CREATE TABLE `bars` (`coor_hash` TEXT NOT NULL UNIQUE,	`name` TEXT NOT NULL, `menu` TEXT, PRIMARY KEY(`coor_hash`));",
     'homes': "CREATE TABLE `homes` (`coor_hash`	TEXT NOT NULL UNIQUE, `detail` TEXT, PRIMARY KEY(`coor_hash`));"
 }
