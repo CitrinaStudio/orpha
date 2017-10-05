1037f4f1f459ae28072fc67bdf3ba450ce07abb6
Phon Phey <phonphey@o2.pl>
2017-10-05 22:54:02 +0300

    000

--- orphah/inside/Player.hx
+++ orphah/inside/Player.hx
@@ -9,26 +9,36 @@ import HaxeLow;
 import Sys;
 import haxe.io.Input;
 
+typedef Params = {
+  pName: String, pAge: Int,
+  pClass: String, pHp: Int,
+  pMp: Int, pStr: Int,
+  pDex: Int, pCon: Int,
+  pInt: Int, pWis: Int,
+  pCha: Int};
 
 class Player {
 
-    public static function new_player() : Dynamic { //Creating new player
-        Sys.print("Input your name: ");
-        var userInput:String;
-        userInput = Sys.stdin().readLine();
-        Sys.println('Your name is ${userInput}');
+    public function new() { //Creating new player
         var db = new HaxeLow('db.json');
         var players = db.col(TablePlayer);
-        Sys.println(db);
-        Sys.println(players);
-        players.push(new TablePlayer("Test", 1977, 54, 68, 78, 56, 65, 68 , 87, 8, 89));
+        var player_params:Params = {pName: "", pAge: 0, pClass: "", pHp: 0, pMp: 0, pStr: 0, pDex: 0, pCon: 0, pInt: 0, pWis: 0, pCha: 0};
+
+        Sys.print("Input your name: ");
+        var name:String = Sys.stdin().readLine();
+        player_params.pName = name;
+        Sys.println('Your name is ${name}');
+
+        Sys.println(player_params);
+
+        players.push(new TablePlayer(player_params));
         db.save();
 
-        return 0;
+        this.params = player_params;
     };
     public static function main(): Void {
-        var player_params = {name: "namf", jopa: "ass"};
-        Sys.println(player_params.jopa);
-        new_player();
+        new Player();
     };
+
+    public var params:Dynamic;
 }
--- orphah/orphadb/Table.hx
+++ orphah/orphadb/Table.hx
@@ -2,34 +2,11 @@ package orphadb;
 
 
 class TablePlayer {
-    public function new(pName, pAge, pClass, pHp, pMp, pStr, pDex, pCon, pInt, pWis, pCha){
-        
-        this.pName = pName; this.pAge =  pAge; this.pHp =  pHp; this.pMp = pMp; this.pStr = pStr; 
+    public function new(player_params){
 
-        this.pDex = pDex; this.pCon = pCon; this.pInt = pInt; this.pWis -pWis; this.pCha = pCha;
+        this.pName = player_params.pName; this.pAge = player_params.pAge; this.pHp = player_params.pHp; this.pMp = player_params.pMp; this.pStr = player_params.pStr;
 
-
-    };
-    public var pName:String;
-    public var pAge:Int;
-    public var pClass:String;
-    public var pHp:Int;
-    public var pMp:Int;
-    public var pStr:Int;
-    public var pDex:Int;
-    public var pCon:Int;
-    public var pInt:Int;
-    public var pWis:Int;
-    public var pCha:Int;
-}
-
-
-class TableEnemy {
-    public function new(pName, pAge, pClass, pHp, pMp, pStr, pDex, pCon, pInt, pWis, pCha){
-        
-        this.pName = pName; this.pAge =  pAge; this.pHp =  pHp; this.pMp = pMp; this.pStr = pStr; 
-
-        this.pDex = pDex; this.pCon = pCon; this.pInt = pInt; this.pWis -pWis; this.pCha = pCha;
+        this.pDex = player_params.pDex; this.pCon = player_params.pCon; this.pInt = player_params.pInt; this.pWis = player_params.pWis; this.pCha = player_params.pCha;
 
 
     };
