d5ab98a4b7b7027755c33d6ff211997405409f17
Oksovskii <21ololoha@gmail.com>
2017-10-05 21:23:49 +0300

    123

--- orphah/inside/Player.hx
+++ orphah/inside/Player.hx
@@ -9,13 +9,24 @@ import Sys;
 import haxe.io.Input;
 
 class Player {
+
     public static function new_player() : Dynamic { //Creating new player
-        Sys.print("Input name: ");
+        Sys.print("Input your name: ");
+        var userInput:String;
+        userInput = Sys.stdin().readLine();
+        Sys.println('Your name is ${userInput}');
+        var db = new HaxeLow('db.json');
+        var players = db.col(Player);
+        Sys.println(db);
+        Sys.println(players);
+        players.push(new Player("Test", 1977));
+        db.save();
 
         return 0;
     };
     public static function main(): Void {
         var player_params = {name: "namf", jopa: "ass"};
         Sys.println(player_params.jopa);
+        new_player();
     };
 }
